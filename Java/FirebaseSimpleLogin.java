90759d48489702f03934adbac80d29d8bd3b1341
Henrique Martins <henrique.m_s@hotmail.com>
2017-08-14 13:51:15 -0300

    Create FirebaseSimpleLogin.java

---
+++ Java/FirebaseSimpleLogin.java
@@ -0,0 +1,44 @@
+package souza.fs.com.flogin;
+
+import android.support.annotation.NonNull;
+import android.support.v4.os.IResultReceiver;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.signInWithEmailAndPassword("henrique@gmail.com","a12345")
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Log.i("ok", "sucesso");
+                        }else
+                            {
+                                Log.i("ok", "falhou");
+                            }
+
+                    }
+                });
+
+//
+
+    }
+}
